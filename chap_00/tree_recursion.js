9556f7e8f85e14cf310c1024d4ee26c8013abedb
cesaraugusto316 <riveramirandac@gmail.com>
2024-01-04 16:06:49 +0000

    🚑 Refactor fib function and add fib2 function

--- chap_00/tree_recursion.js
+++ chap_00/tree_recursion.js
@@ -1,7 +1,43 @@
-export function fib(n) {
-  return (
-    n === 0 ? 0
-      : n === 1 ? 1
-        : fib(n - 1) + fib(n - 2)
-  );
+'use strict';
+
+/**
+ *
+ * @param {number} n
+ * @yields {number}
+ */
+export function* fib(n) {
+  const iter = (x) => {
+    return (
+      x === 0 ? 0
+        : x === 1 ? 1
+          : x - 1 + x - 2
+    );
+  };
+
+  for (let i = 0; i <= n; i++) {
+    yield iter(i);
+  }
 }
+
+
+/**
+ *
+ * @param {number} n
+ * @returns {number}
+ */
+export function fib2(n) {
+  const iter = (a, b, count) => {
+    return count === 0
+      ? b
+      : iter(a + b, a, count--);
+  };
+
+  return iter(1, 0, n);
+}
+
+// console.log(fib(10));c
+// console.log(fibNumbers.next());
+// console.log(fibNumbers.next());
+// console.log(fibNumbers.next());
+
+console.log(fib2(7));
